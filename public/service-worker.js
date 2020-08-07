fd135d00d29fe213eeecae02f2002dd382ea8adc
Linnea <linneagear@gmail.com>
2020-08-06 19:53:22 -0700

    Adding service worker install and activate

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,8 +12,39 @@ const STATIC_CACHE = "static-cache-v1";
 const RUNTIME_CACHE = "runtime-cache";
 
 // used boilerplate code from previous activity:
-
-self.addEventListener("fetch", event => {
+self.addEventListener("install", event => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+  });
+  
+  // The activate handler takes care of cleaning up old caches.
+  self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames => {
+          // return array of cache names that are old to delete
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
+        })
+        .then(cachesToDelete => {
+          return Promise.all(
+            cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+  
+  self.addEventListener("fetch", event => {
     // non GET requests are not cached and requests to other origins are not cached
     if (
       event.request.method !== "GET" ||
