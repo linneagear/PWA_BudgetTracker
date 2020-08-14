5d6d0de00e200d2f4d655135227ec968117cf453
Linnea <linneagear@gmail.com>
2020-08-13 20:03:24 -0700

    Fixed indexedDB to correct budget db to match

--- routes/api.js
+++ routes/api.js
@@ -1,37 +1,35 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-// post body of user's transaction to api
-router.post("/api/transaction", ({body}, res) => {
+router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
     .catch(err => {
-      res.status(404).json(err);
+      res.status(400).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
     .catch(err => {
-      res.status(404).json(err);
+      res.status(400).json(err);
     });
 });
 
-// find all transactions at descending date
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
+  Transaction.find({})
+    .sort({ date: -1 })
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
     .catch(err => {
-      res.status(404).json(err);
+      res.status(400).json(err);
     });
 });
 
-// export our router to be used elsewhere
 module.exports = router;
